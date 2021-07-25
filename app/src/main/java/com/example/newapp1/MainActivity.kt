3fcdd5f551e5a9a2c8ce7c650a1cd4920e8d9274
siddhihandal <siddhinh17.extc@coep.ac.in>
2021-07-25 19:25:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newapp1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newapp1
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import java.util.*
